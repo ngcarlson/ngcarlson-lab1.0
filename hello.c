146db568652abe970ba91feb088c59a59f7bedd3
ngcarlson <42647317+ngcarlson@users.noreply.github.com>
2018-08-24 16:29:26 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nolan Carlson
+ * Date: 2016/11/02
+ *
+ * A not-so simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nolan Carlson, Mechanical Engineering!\n");
+
+  return 0;
+}
